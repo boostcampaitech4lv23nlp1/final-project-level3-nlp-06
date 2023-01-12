3fad1e70edefddd3e0dcf8e9f4b72960c26a0aac
intrandom5 <random_5@naver.com>
2023-01-12 14:10:33 +0000

    feat: add imports from model

--- classification/main.py
+++ classification/main.py
@@ -1,8 +1,5 @@
-import torch
-from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
-from sklearn.metrics import accuracy_score, roc_auc_score
-import wandb
 import yaml
+import torch
 import argparse
 
 from data import Apeach_Dataset, kmhas_Dataset, KOLD_Dataset
@@ -11,27 +8,16 @@ from trainer import HuggingfaceTrainer, CNNTrainer
 
 
 Dataset = {"APEACH": Apeach_Dataset, "kmhas": kmhas_Dataset, "kold": KOLD_Dataset}
+models = {"CNN": CNNModel, "multi_label": multi_label_bert, "bi_label": bi_label_bert}
 trainer = {"huggingface": HuggingfaceTrainer, "cnn": CNNTrainer}
 
-
 def main(config):
-    if config["multi_label"]:
-        model = AutoModelForSequenceClassification.from_pretrained(
-            config["model_name"], 
-            num_labels=config["num_labels"], 
-            problem_type="multi_label_classification"
-        )
-    else:
-        model = AutoModelForSequenceClassification.from_pretrained(
-            config["model_name"], 
-            num_labels=config["num_labels"]
-        )
-
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model.to(device)
 
     train_dataset = Dataset[config["dataset"]](config["train_dir"], config["model_name"])
     valid_dataset = Dataset[config["dataset"]](config["valid_dir"], config["model_name"])
+    model = models[config["model"]](config, train_dataset.tokenizer.vocab_size)
+    model.to(device)
     
     trainer = trainer[config["trainer"]](config, model, train_dataset, valid_dataset)
 
