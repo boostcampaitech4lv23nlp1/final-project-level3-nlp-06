f6a89c10291dd2bb7e47ed172e388844cf30db2b
intrandom5 <random_5@naver.com>
2023-01-12 14:23:32 +0000

    fix: fix inference.py

--- classification/inference.py
+++ classification/inference.py
@@ -1,52 +1,46 @@
 import torch
-from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+from torch.utils.data import DataLoader
 import yaml
 import argparse
-import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from sklearn.metrics import f1_score
 
-from data import Apeach_Dataset, kmhas_Dataset
+from data import Apeach_Dataset, kmhas_Dataset, KOLD_Dataset
+from model import bi_label_bert, multi_label_bert, CNNModel
 from utils import Compute_metrics, get_prediction
 
 
-Dataset = {"APEACH": Apeach_Dataset, "kmhas": kmhas_Dataset}
+Dataset = {"APEACH": Apeach_Dataset, "kmhas": kmhas_Dataset, "kold": KOLD_Dataset}
+models = {"CNN": CNNModel, "multi_label": multi_label_bert, "bi_label": bi_label_bert}
 
 def main(config):
-    if config["multi_label"]:
-        model = AutoModelForSequenceClassification.from_pretrained(
-            config["checkpoint_dir"], 
-            num_labels=config["num_labels"], 
-            problem_type="multi_label_classification"
-        )
-    else:
-        model = AutoModelForSequenceClassification.from_pretrained(
-            config["checkpoint_dir"], 
-            num_labels=config["num_labels"]
-        )
-
+    valid_dataset = Dataset[config["dataset"]](config["valid_dir"], config["model_name"])
+    valid_loader = DataLoader(valid_dataset, batch_size=config["batch_size"])
+    model = models[config["model"]](config, valid_dataset.tokenizer.vocab_size)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
-
-    valid_dataset = Dataset[config["dataset"]](config["valid_dir"], config["model_name"])
-
-    CM = Compute_metrics(multi_label=config["multi_label"], num_labels=config["num_labels"])
-    compute_metrics = CM.compute_metrics
-        
-    trainer = Trainer(
-        model=model,
-        compute_metrics=compute_metrics
-    )
-
-    result = trainer.predict(valid_dataset)
-    print(result.metrics)
     
-    preds = get_prediction(result, config["label_map_dir"], config["multi_label"])
-    
-    df = valid_dataset.df
-    df["pred"] = preds
-    probs = torch.sigmoid(torch.tensor(result.predictions))
-    df["probs"] = [str(np.round(p, 2).tolist()) for p in probs.tolist()]
-    df.to_csv(config["result_dir"])
-    
+    preds = []
+    model.eval()
+    for data in tqdm(valid_loader):
+        batch_size = data['input_ids'].shape[0]
+        data = {k: v.to('cuda') for k, v in data.items()}
+        label = data.pop('label')
+        with torch.no_grad():
+            if config["model"] == "CNN":
+                outputs = model(data).reshape(batch_size, 1)
+            else:
+                outputs = model(**data).logits
+        preds += [output.item() for output in outputs.cpu()]
+    f1 = f1_score(preds, valid_dataset.labels)
+    print("f1 score :", f1)
+    df = {
+        "sentence": valid_dataset.sentences,
+        "label": valid_dataset.labels,
+        "preds": preds
+    }
+    pd.DataFrame.from_dict(df).to_csv(config["result_dir"], index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Hate Speech Classification')
