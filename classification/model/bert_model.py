4e7dd8d76fc538e913a1482ccce7ae285f78afaa
intrandom5 <random_5@naver.com>
2023-01-12 14:10:08 +0000

    feat: add vocab_size option in bert_model.py for convinience

--- classification/model/bert_model.py
+++ classification/model/bert_model.py
@@ -1,14 +1,14 @@
 from transformers import AutoModelForSequenceClassification
 
 
-def multi_label_bert(config):
+def multi_label_bert(config, vocab_size=None):
     return AutoModelForSequenceClassification.from_pretrained(
         config["model_name"],
         num_labels=config["num_labels"],
         problem_type="multi_label_classification"
     )
     
-def bi_label_bert(config):
+def bi_label_bert(config, vocab_size=None):
     return AutoModelForSequenceClassification.from_pretrained(
         config["model_name"],
         num_labels=config["num_labels"],
