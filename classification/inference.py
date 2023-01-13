cf3fe0641f699f81027f343c8f39014ab43ddb7e
intrandom5 <random_5@naver.com>
2023-01-13 04:55:57 +0000

    fix: validate inference.py

--- classification/inference.py
+++ classification/inference.py
@@ -6,11 +6,11 @@ import pandas as pd
 from tqdm import tqdm
 from sklearn.metrics import f1_score
 
-from data import Apeach_Dataset, kmhas_Dataset, KOLD_Dataset
+from data import Apeach_Dataset, kmhas_Dataset, KOLD_Dataset, Unsmile_Dataset, Beep_Dataset
 from model import CNNModel, transformer
 
 
-Dataset = {"APEACH": Apeach_Dataset, "k-mhas": kmhas_Dataset, "KOLD": KOLD_Dataset}
+Dataset = {"APEACH": Apeach_Dataset, "BEEP!": Beep_Dataset, "Unsmile": Unsmile_Dataset, "k-mhas": kmhas_Dataset, "KOLD": KOLD_Dataset}
 models = {"CNN": CNNModel, "Transformer": transformer}
 
 def main(config):
@@ -19,24 +19,25 @@ def main(config):
     model = models[config["model"]](config, valid_dataset.tokenizer.vocab_size)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
+    model.load_state_dict(torch.load(config["checkpoint_dir"]))
     
     preds = []
     model.eval()
     for data in tqdm(valid_loader):
-        batch_size = data['input_ids'].shape[0]
-        data = {k: v.to('cuda') for k, v in data.items()}
+        data = {k: v.to(device) for k, v in data.items()}
         label = data.pop('label')
         with torch.no_grad():
             if config["model"] == "CNN":
-                outputs = model(data).reshape(batch_size, 1)
+                outputs = model(data['input_ids']).squeeze()
             else:
                 outputs = model(**data).logits
-        preds += [output.item() for output in outputs.cpu()]
-    f1 = f1_score(preds, valid_dataset.labels)
+        preds += [1 if output.item() > 0.5 else 0 for output in outputs.cpu()]
+        
+    f1 = f1_score(preds, valid_dataset.label)
     print("f1 score :", f1)
     df = {
-        "sentence": valid_dataset.sentences,
-        "label": valid_dataset.labels,
+        "sentence": valid_dataset.text,
+        "label": valid_dataset.label,
         "preds": preds
     }
     pd.DataFrame.from_dict(df).to_csv(config["result_dir"], index=False)
