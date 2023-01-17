2c8a8e78283e8b5eb5905938a424f2991fe0c057
intrandom5 <random_5@naver.com>
2023-01-17 09:58:57 +0000

    fix: make right cnn model

--- classification/model/cnn_model.py
+++ classification/model/cnn_model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from transformers import AutoModel
 
@@ -39,22 +40,41 @@ class CNNModel(nn.Module):
         return self.Layer(x)
     
 
-class VerifiableCNN(CNNModel):
+class VerifiableCNN(nn.Module):
     def __init__(self, config, vocab_size):
-        super(VerifiableCNN, self).__init__(config, vocab_size)
+        super(VerifiableCNN, self).__init__()
+        
+        self.config = config
+        
+        self.Embedding = nn.Embedding(vocab_size, 768)
+        if self.config["get_bert_embedding"]:
+            embedding_weights = self.prepare_embeddings()
+            self.Embedding.weight = embedding_weights
         self.Layer = nn.Sequential(
-            self.conv_block(128, 128), # 384
-            self.conv_block(128, 128), # 192
-            self.conv_block(128, 128), # 96
-            nn.Linear(96, 1),
+            nn.Conv1d(768, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv1d(128, 1, kernel_size=3, padding=1),
+            nn.ReLU(),
         )
-        self.fc = nn.Linear(128, 1)
         self.sigmoid = nn.Sigmoid()
         
-    def forward(self, inputs):
+    def prepare_embeddings(self):
+        transformer = AutoModel.from_pretrained(self.config["model_name"])
+        for name, param in transformer.named_parameters():
+            if "embeddings.word_embeddings" in name:
+                embedding_weights = param
+        del transformer
+        return embedding_weights
+    
+    def mean_pooling(self, token_embeddings, attention_mask):
+        input_mask_expanded = attention_mask.expand(token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        
+    def forward(self, inputs, mask):
         x = self.Embedding(inputs)
+        x = x.transpose(1, 2)
         x = self.Layer(x)
-        x = x.squeeze(-1)
-        x = self.fc(x)
-        return self.sigmoid(x)
+        pooled_outputs = self.mean_pooling(x.squeeze(1), mask)
+        pooled_outputs = self.sigmoid(pooled_outputs)
+        return pooled_outputs
         
