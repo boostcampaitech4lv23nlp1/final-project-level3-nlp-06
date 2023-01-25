ccf9aea186c0e4e78d482179e59e2b8085d10ccd
intrandom5 <random_5@naver.com>
2023-01-25 04:31:46 +0000

    feat: add Sequence_Span_Dataset

--- classification/data/__init__.py
+++ classification/data/__init__.py
@@ -1,3 +1,3 @@
 from .dataset import Apeach_Dataset, kmhas_Dataset, KOLD_Dataset, Beep_Dataset, Unsmile_Dataset
 from .tokenization_kocharelectra import KoCharElectraTokenizer
-from .span_dataset import Span_Dataset
+from .span_dataset import Span_Dataset, Sequence_Span_Dataset
