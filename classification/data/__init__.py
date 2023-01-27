c7244b979b357456d4d1e075eb4b43fad02273fc
intrandom5 <random_5@naver.com>
2023-01-27 02:08:26 +0000

    feat: add multi-label module in __init__.py

--- classification/data/__init__.py
+++ classification/data/__init__.py
@@ -1,3 +1,3 @@
-from .dataset import Apeach_Dataset, kmhas_Dataset, KOLD_Dataset, Beep_Dataset, Unsmile_Dataset
+from .dataset import Apeach_Dataset, kmhas_Dataset, KOLD_Dataset, Beep_Dataset, Unsmile_Dataset, kmhas_multilabel_Dataset
 from .tokenization_kocharelectra import KoCharElectraTokenizer
 from .span_dataset import Span_Dataset, Sequence_Span_Dataset
