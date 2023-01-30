287823ea40666b99842897574e5950bcc5f8df7b
intrandom5 <random_5@naver.com>
2023-01-30 12:41:23 +0000

    feat: add model queue

--- classificationAPI/app.py
+++ classificationAPI/app.py
@@ -1,5 +1,7 @@
 import yaml
+import time
 import uvicorn
+from collections import deque
 from fastapi import FastAPI, Request
 from model import ClassificationModel
 from fastapi.middleware.cors import CORSMiddleware
@@ -7,7 +9,9 @@ from fastapi.middleware.cors import CORSMiddleware
 ## Setting Config and Model
 with open("config.yaml") as f:
     config = yaml.load(f, Loader=yaml.Loader)
-model = ClassificationModel(config)
+
+models = deque([(ClassificationModel(config), i) for i in range(4)])
+# model = ClassificationModel(config)
 
 ## Start fastapi server.
 app = FastAPI()
@@ -22,12 +26,19 @@ app.add_middleware(
 
 @app.get("/classification")
 async def classifying(request: Request, comments: str):
+    while len(models) == 0:
+        time.sleep(0.1)
+    
+    model, index = models.pop()
+
     hate, token_output, tokenized_sentence = model.predict(comments)
     result = {
         "is_hate": hate,
         "token_hate": token_output,
         "tokenized_sentence": tokenized_sentence
     }
+    models.appendleft((model, index))
+    
     return result
 
 # if __name__=="__main__":
